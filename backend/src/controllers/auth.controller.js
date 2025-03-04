5eb4796c1dd7642ea3fb622eb3ae979fcdaa89bb
Imran Khan <khanimran092606@gmail.com>
2025-03-04 02:48:32 -0500

    update profile  pic & protectRoutes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
+import cloudinary from "../lib/cloudinary.js"
+
 export const signup = async (req, res) => {
     const {fullName, email, password} = req.body;
     try {
@@ -74,5 +76,25 @@ export const logout = (req, res) => {
         console.log("Error in Logout Controller", error.message)       
         res.status(500).json({message:"Internal server error"})
 
+    }
+};
+
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePic} = req.body
+        const userId = req.user._id
+
+        if(!profilePic){
+            return res.status(400).json({message: "profile pic is required"})
+        }
+
+       const uploadResponse = await cloudinary.uploader.upload(profilePic)
+
+       const updatedUser = await User.findByIdAndUpdate(userId, {profilepic:uploadResponse.secure_url}, {new:true})
+       res.status(200).json({message:"updates user"})
+
+    } catch (error) {
+        console.log("error in the updated profile", error.message)
+        res.status(500).json({message:"internal server error"})
     }
 };
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,10 +2,13 @@ import express from "express"
 import { signup } from "../controllers/auth.controller.js";
 import { login } from "../controllers/auth.controller.js";
 import { logout } from "../controllers/auth.controller.js";
+import { updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js"
 
 const router = express.Router()
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+router.put("/update-profile", protectRoute, updateProfile)
 export default router;
